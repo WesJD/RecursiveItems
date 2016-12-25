16188680be826ff60df721d085679342362b0201
WesJD <wesjavadev@gmail.com>
2016-12-25 17:06:12 -0500

    Cleanup
    Also fixes an IndexOutOfBoundsException in SetWorthCommand

--- src/main/java/net/wesjd/recursiveitems/command/RemoveWorthCommand.java
+++ src/main/java/net/wesjd/recursiveitems/command/RemoveWorthCommand.java
@@ -3,17 +3,21 @@ package net.wesjd.recursiveitems.command;
 import net.wesjd.recursiveitems.RecursiveItems;
 import net.wesjd.recursiveitems.util.ParsingUtils;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 /**
  * /removeworth command, removes the worth of an ItemStack
  *
- * @author Daniel Gongora
+ * @author Daniel Gongora, Wesley Smith
  */
 public class RemoveWorthCommand extends AbstractCommand {
 
-    private static final String WORTH_REMOVE = ChatColor.GREEN + "%s's%s worth has been removed.";
+    /**
+     * Format used when sending a message to the player in {@link RemoveWorthCommand#onCmd(Player, String[])}
+     */
+    private static final String WORTH_REMOVE_FORMAT = ChatColor.GREEN + "%s's%s worth has been removed.";
 
     /**
      * @inheritDoc
@@ -27,55 +31,24 @@ public class RemoveWorthCommand extends AbstractCommand {
      */
     @Override
     public void onCmd(Player player, String[] args) throws Exception {
+        final boolean isDefault = args.length > 0 && args[0].equalsIgnoreCase("default");
+        final int materialIndex = isDefault ? 1 : 0;
+
         ItemStack toWorth;
-        boolean isDefault;
-        int index = 0;
+        if (args.length - materialIndex == 1) //item was supplied
+            toWorth = ParsingUtils.getStackFromArg(args[materialIndex]);
+        else toWorth = player.getItemInHand();
 
-        if (args.length > 0 && args[index].equalsIgnoreCase("default")) {//if they want the default, increase the index, and mark it was default
-            index++;
-            isDefault = true;
-        } else isDefault = false;
-
-        player.sendMessage("THINGITNHITNG: "+(args.length - index));
-
-        if (args.length - index == 1) { //item was supplied
-            toWorth = ParsingUtils.getStackFromArg(args[index]);
-            player.sendMessage("ayayaayaye");
-        } else toWorth = player.getItemInHand();
-
-        if (toWorth == null) {
-            showUsage(player);
-            return;
+        if (toWorth == null || toWorth.getType() == Material.AIR) showUsage(player);
+        else {
+            if(isDefault) super.main.getEngine().removeDefaultWorth(toWorth);
+            else super.main.getEngine().removeWorth(toWorth);
+            player.sendMessage(String.format(
+                    WORTH_REMOVE_FORMAT,
+                    toWorth.getType() + (toWorth.getDurability() > 0 ? ":" + toWorth.getDurability() : ""),
+                    (isDefault ? " default" : "")
+            ));
         }
-
-        String extra = "";
-        System.out.println("removing worth for: "+(toWorth.getType() + (toWorth.getDurability() > 0 ? ":" + toWorth.getDurability() : "")));
-
-        if (isDefault) {
-            super.main.getEngine().removeDefaultWorth(toWorth);
-            extra = " default"; //make it say "..GRASS's default worth has been.." instead of "..GRASS's worth has been.."
-        } else super.main.getEngine().removeWorth(toWorth);
-
-        player.sendMessage(String.format(WORTH_REMOVE,
-                toWorth.getType() + (toWorth.getDurability() > 0 ? ":" + toWorth.getDurability() : ""),
-                extra)); //tell them a beautiful message
-/*
-        if (args.length == 0) {
-            final ItemStack hand = player.getItemInHand();
-            if (hand != null) toWorth = hand;
-            else {
-                player.sendMessage(ChatColor.RED + "Usage: /removeworth [default] [material[:dataValue]]");
-                return;
-            }
-        } else if (args.length < 2) {
-            toWorth = ParsingUtils.getStackFromArg(args[0]);
-        } else {
-            player.sendMessage(ChatColor.RED + "Too many arguments.");
-            return;
-        }
-
-        super.main.getEngine().removeWorth(toWorth);
-        player.sendMessage(ChatColor.GREEN.toString() + toWorth.getType() + (toWorth.getDurability() > 0 ? ":" + toWorth.getDurability() : "") + "'s worth has been removed.");*/
     }
 
     private void showUsage(Player player) {
